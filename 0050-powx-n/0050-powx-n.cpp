7724d9238e70cb410247822044667cc2c97710a5
Rohit <rohit.gopu00@gmail.com>
2026-07-28 18:14:16 +0530

    Time: 1 ms (3.84%), Space: 8.7 MB (12.8%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        long int N = n;
+
+        if(N<0){
+           x = 1/x;
+            N = -N;
+        }
+        
+        
+        double ans=1;
+        while(N){
+            if(N%2==1){
+                ans *=x;
+            }
+            x = x*x;
+            N /=2;
+        }
+        return ans;
+    }
+};
